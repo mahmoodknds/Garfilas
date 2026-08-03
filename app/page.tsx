69ac9fc2df06eb3ff9c49af78ae467936aded600
mahmoodknds <mahmood.knds@gmail.com>
2026-08-04 00:54:16 +0330

    feat: compose landing page hero and bottom navigation

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,11 @@
+import BottomNavigation from "@/components/sections/BottomNavigation/BottomNavigation";
 import HeroSection from "@/components/sections/Hero/HeroContent";
 
 export default function Home() {
   return (
-    <main>
+    <main id="hero">
       <HeroSection />
+      <BottomNavigation />
     </main>
   );
 }
