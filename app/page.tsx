85dfd39d854f1d146195943283b6564d27baeb6c
mahmoodknds <mahmood.knds@gmail.com>
2026-08-04 18:42:49 +0330

    feat: connect featured products to landing page

--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,12 @@
 import BottomNavigation from "@/components/sections/BottomNavigation/BottomNavigation";
+import FeaturedProducts from "@/components/sections/FeaturedProducts";
 import HeroSection from "@/components/sections/Hero/HeroContent";
 
 export default function Home() {
   return (
     <main id="hero">
       <HeroSection />
+      <FeaturedProducts />
       <BottomNavigation />
     </main>
   );
