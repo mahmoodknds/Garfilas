b015801ba27dc227ce153404184f70cd3a803505
mahmoodknds <mahmood.knds@gmail.com>
2026-08-04 18:50:11 +0330

    feat: connect Story section to landing page

--- app/page.tsx
+++ app/page.tsx
@@ -1,12 +1,14 @@
 import BottomNavigation from "@/components/sections/BottomNavigation/BottomNavigation";
 import FeaturedProducts from "@/components/sections/FeaturedProducts";
 import HeroSection from "@/components/sections/Hero/HeroContent";
+import Story from "@/components/sections/Story";
 
 export default function Home() {
   return (
     <main id="hero">
       <HeroSection />
       <FeaturedProducts />
+      <Story />
       <BottomNavigation />
     </main>
   );
