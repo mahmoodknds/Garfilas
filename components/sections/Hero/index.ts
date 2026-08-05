9b7df3a681bc5ff05a6350e6559ad61458a10092
mahmoodknds <mahmood.knds@gmail.com>
2026-08-06 03:07:35 +0330

    refactor(hero): export hero composition components

---
+++ components/sections/Hero/index.ts
@@ -0,0 +1,4 @@
+export { default } from "./Hero";
+export { default as HeroLogo } from "./HeroLogo";
+export { default as HeroContent } from "./HeroContent";
+export { default as HeroCTA } from "./HeroCTA";
